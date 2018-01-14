89a0b326a209787bb141aa68cf74b99f8f777508
yoshzawa <t.yoshizawa@jc-21.jp>
2018-01-14 16:03:19 +0900

    initial commit

--- NetGame/NetGame/MainPage.xaml.cs
+++ NetGame/NetGame/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -12,6 +13,27 @@ namespace NetGame
 		public MainPage()
 		{
 			InitializeComponent();
+            buttonJisaku.Clicked += ButtonJisaku_Clicked;
+            buttonOtehon.Clicked += ButtonOtehon_Clicked;
 		}
-	}
+
+        private async void ButtonOtehon_Clicked(object sender, EventArgs e)
+        {
+            String url = "https://h29netgame.azurewebsites.net/Login?id=5";
+            HttpClient client = new HttpClient();
+            var result = await client.GetAsync(url);
+            if (result.IsSuccessStatusCode)
+            {
+                var content = await result.Content.ReadAsStringAsync();
+//                Items = JsonConvert.DeserializeObject<List<TodoItem>>(content);
+            }
+
+
+        }
+
+        private void ButtonJisaku_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
